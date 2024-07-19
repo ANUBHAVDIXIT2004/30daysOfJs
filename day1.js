489a5d45a30186c866967830cda54123ec6deede
ANUBHAVDIXIT2004 <anubhavdixit1362012@gmail.com>
2024-07-19 21:02:45 +0530

    added day1.js file

---
+++ day1.js
@@ -0,0 +1,53 @@
+// activity 1 
+
+//task1
+
+var num1=70;
+console.log(num1);//output= 70
+
+//task2
+
+let str1="hello ,kaise ho aap log ?";
+console.log(str1);//output=hello ,kaise ho aap log ?
+
+//activity 2
+
+//task3
+
+var bool1=false;
+console.log(bool1);//output=false
+
+//activity 3
+
+//task4
+
+let num = 70; // Number
+let str = "ANUBHAV DIXIT"; // String
+let bool = false; // Boolean
+let obj = {name: "aditya", Number : 6}; // Object
+let arr = [4,5,6,7,8]; // Array
+
+console.log(typeof num); // Output: number
+console.log(typeof str); // Output: string
+console.log(typeof bool); // Output: boolean
+console.log(typeof obj); // Output: object
+console.log(typeof arr); // Output: object (Arrays are a type of object in JavaScript)
+
+//activity 4
+
+//task5
+
+let varold="old";
+console.log(varold);// Output:old
+varold="new";
+console.log(varold);// Output:new
+
+//activity 5
+
+//task6
+
+const constVar = "Constant Value";
+console.log(constVar); // Output: Constant Value
+// Uncommenting the next line will cause an error
+// constVar = "New Value"; // Error: Assignment to constant variable.
+
