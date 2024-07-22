92088b259a9d7f7e730c8759003af87957e4b57c
ANUBHAVDIXIT2004 <anubhavdixit1362012@gmail.com>
2024-07-22 14:11:30 +0530

    completed day4.js file

--- day4.js
+++ day4.js
@@ -115,3 +115,40 @@ for (let m = 1; m <= rows; m++) {
 // ***
 // ****
 // *****
+
+// Activity 5: Loop Control Statements
+
+// Print numbers from 1 to 10, but skip the number 5 using the continue statement
+for (let i = 1; i <= 10; i++) {
+    if (i === 5) {
+        continue;
+    }
+    console.log(i);
+}
+
+// Output:
+// 1
+// 2
+// 3
+// 4
+// 6
+// 7
+// 8
+// 9
+// 10
+
+// Print numbers from 1 to 10 but stop the loop when the number is 7 using the break statement
+for (let i = 1; i <= 10; i++) {
+    if (i === 7) {
+        break;
+    }
+    console.log(i);
+}
+
+// Output:
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
